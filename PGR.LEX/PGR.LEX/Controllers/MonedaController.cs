a5af7ce6569051c386d2a60f1834148266ef0964
HumbertoPGR <luispg@pgr.go.cr>
2018-11-30 15:11:48 -0600

    Se agregan los mantenimientos tipo de moneda y tipo de audiencia

---
+++ PGR.LEX/PGR.LEX/Controllers/MonedaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PGR.LEX.Models;
+
+namespace PGR.LEX.Controllers
+{
+    public class MonedaController : Controller
+    {
+        private LitigiosoEntities db = new LitigiosoEntities();
+
+        // GET: Moneda
+        public ActionResult Index()
+        {
+            return View(db.TMoneda.ToList());
+        }
+
+        // GET: Moneda/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TMoneda tMoneda = db.TMoneda.Find(id);
+            if (tMoneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tMoneda);
+        }
+
+        // GET: Moneda/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Moneda/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idMoneda,Descripcion")] TMoneda tMoneda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TMoneda.Add(tMoneda);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tMoneda);
+        }
+
+        // GET: Moneda/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TMoneda tMoneda = db.TMoneda.Find(id);
+            if (tMoneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tMoneda);
+        }
+
+        // POST: Moneda/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idMoneda,Descripcion")] TMoneda tMoneda)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tMoneda).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tMoneda);
+        }
+
+        // GET: Moneda/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TMoneda tMoneda = db.TMoneda.Find(id);
+            if (tMoneda == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tMoneda);
+        }
+
+        // POST: Moneda/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TMoneda tMoneda = db.TMoneda.Find(id);
+            db.TMoneda.Remove(tMoneda);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ PGR.LEX/PGR.LEX/Controllers/TipoAudienciaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PGR.LEX.Models;
+
+namespace PGR.LEX.Controllers
+{
+    public class TipoAudienciaController : Controller
+    {
+        private LitigiosoEntities db = new LitigiosoEntities();
+
+        // GET: TipoAudiencia
+        public ActionResult Index()
+        {
+            return View(db.TTipoAudiencia.ToList());
+        }
+
+        // GET: TipoAudiencia/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TTipoAudiencia tTipoAudiencia = db.TTipoAudiencia.Find(id);
+            if (tTipoAudiencia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tTipoAudiencia);
+        }
+
+        // GET: TipoAudiencia/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TipoAudiencia/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idTipoAudiencia,Descripcion")] TTipoAudiencia tTipoAudiencia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TTipoAudiencia.Add(tTipoAudiencia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tTipoAudiencia);
+        }
+
+        // GET: TipoAudiencia/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TTipoAudiencia tTipoAudiencia = db.TTipoAudiencia.Find(id);
+            if (tTipoAudiencia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tTipoAudiencia);
+        }
+
+        // POST: TipoAudiencia/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idTipoAudiencia,Descripcion")] TTipoAudiencia tTipoAudiencia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tTipoAudiencia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tTipoAudiencia);
+        }
+
+        // GET: TipoAudiencia/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TTipoAudiencia tTipoAudiencia = db.TTipoAudiencia.Find(id);
+            if (tTipoAudiencia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tTipoAudiencia);
+        }
+
+        // POST: TipoAudiencia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TTipoAudiencia tTipoAudiencia = db.TTipoAudiencia.Find(id);
+            db.TTipoAudiencia.Remove(tTipoAudiencia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
